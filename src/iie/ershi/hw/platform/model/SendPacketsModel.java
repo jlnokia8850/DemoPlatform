40a1168e6a8553b847fe25c9cadc677ba58000fb
Jiang, Lei <jlnokia8850@gmail.com>
2013-07-20 13:02:47 +0800

    111

--- src/iie/ershi/hw/platform/model/SendPacketsModel.java
+++ src/iie/ershi/hw/platform/model/SendPacketsModel.java
@@ -6,33 +6,34 @@ public class SendPacketsModel {
 	 * 重复发送次数
 	 */
 	private String sendTimes;
-	
-	private String srcMac;
 
-//	/**
-//	 * 源Mac地址
-//	 */
-//	private String srcMac;
-//	
-//	/**
-//	 * 目的Mac地址
-//	 */
-//	private String dstMac;
-//
-//	/**
-//	 * 源IP地址
-//	 */
-//	private String srcIP;
-//
-//	/**
-//	 * 目的IP地址
-//	 */
-//	private String dstIP;
-//
-//	/**
-//	 * 协议号
-//	 */
-//	private String ipPro;
+	private String srcMac;
+	
+
+	// /**
+	// * 源Mac地址
+	// */
+	// private String srcMac;
+	//
+	// /**
+	// * 目的Mac地址
+	// */
+	// private String dstMac;
+	//
+	// /**
+	// * 源IP地址
+	// */
+	// private String srcIP;
+	//
+	// /**
+	// * 目的IP地址
+	// */
+	// private String dstIP;
+	//
+	// /**
+	// * 协议号
+	// */
+	// private String ipPro;
 
 	public String getSrcMac() {
 		return srcMac;
@@ -50,83 +51,83 @@ public class SendPacketsModel {
 		this.sendTimes = sendTimes;
 	}
 
-//	public String getSrcMac() {
-//		return srcMac;
-//	}
-//
-//	public void setSrcMac(String srcMac) {
-//		this.srcMac = srcMac;
-//	}
-//
-//	public String getDstMac() {
-//		return dstMac;
-//	}
-//
-//	public void setDstMac(String dstMac) {
-//		this.dstMac = dstMac;
-//	}
-//
-//	public String getSrcIP() {
-//		return srcIP;
-//	}
-//
-//	public void setSrcIP(String srcIP) {
-//		this.srcIP = srcIP;
-//	}
-//
-//	public String getDstIP() {
-//		return dstIP;
-//	}
-//
-//	public void setDstIP(String dstIP) {
-//		this.dstIP = dstIP;
-//	}
-//
-//	public String getIpPro() {
-//		return ipPro;
-//	}
-//
-//	public void setIpPro(String ipPro) {
-//		this.ipPro = ipPro;
-//	}
-//
-//	public String getSrcPort() {
-//		return srcPort;
-//	}
-//
-//	public void setSrcPort(String srcPort) {
-//		this.srcPort = srcPort;
-//	}
-//
-//	public String getDstPort() {
-//		return dstPort;
-//	}
-//
-//	public void setDstPort(String dstPort) {
-//		this.dstPort = dstPort;
-//	}
-//
-//	public String getDataContent() {
-//		return dataContent;
-//	}
-//
-//	public void setDataContent(String dataContent) {
-//		this.dataContent = dataContent;
-//	}
-//
-//	/**
-//	 * 源端口
-//	 */
-//	private String srcPort;
-//
-//	/**
-//	 * 目的端口
-//	 */
-//	private String dstPort;
-//
-//	/**
-//	 * 数据包内容
-//	 */
-//	private String dataContent;
+	// public String getSrcMac() {
+	// return srcMac;
+	// }
+	//
+	// public void setSrcMac(String srcMac) {
+	// this.srcMac = srcMac;
+	// }
+	//
+	// public String getDstMac() {
+	// return dstMac;
+	// }
+	//
+	// public void setDstMac(String dstMac) {
+	// this.dstMac = dstMac;
+	// }
+	//
+	// public String getSrcIP() {
+	// return srcIP;
+	// }
+	//
+	// public void setSrcIP(String srcIP) {
+	// this.srcIP = srcIP;
+	// }
+	//
+	// public String getDstIP() {
+	// return dstIP;
+	// }
+	//
+	// public void setDstIP(String dstIP) {
+	// this.dstIP = dstIP;
+	// }
+	//
+	// public String getIpPro() {
+	// return ipPro;
+	// }
+	//
+	// public void setIpPro(String ipPro) {
+	// this.ipPro = ipPro;
+	// }
+	//
+	// public String getSrcPort() {
+	// return srcPort;
+	// }
+	//
+	// public void setSrcPort(String srcPort) {
+	// this.srcPort = srcPort;
+	// }
+	//
+	// public String getDstPort() {
+	// return dstPort;
+	// }
+	//
+	// public void setDstPort(String dstPort) {
+	// this.dstPort = dstPort;
+	// }
+	//
+	// public String getDataContent() {
+	// return dataContent;
+	// }
+	//
+	// public void setDataContent(String dataContent) {
+	// this.dataContent = dataContent;
+	// }
+	//
+	// /**
+	// * 源端口
+	// */
+	// private String srcPort;
+	//
+	// /**
+	// * 目的端口
+	// */
+	// private String dstPort;
+	//
+	// /**
+	// * 数据包内容
+	// */
+	// private String dataContent;
 
 }
